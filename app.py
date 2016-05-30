b47c7bde01875895678ee93b259e35a4810ff2fb
Mitch Anderson <mitch@nuvi.com>
2016-05-30 09:48:08 -0600

    Trying to get proper error wrapping around mongoDB connection.  Not printing all logging but works from my testing

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import requests
+import pymongo
+import random
+from time import sleep
+import os
+
+NAME = 'giles'
+VERSION = '0.1'
+
+MONGO_URI = 'mongodb://mongo:27017'
+
+app = Flask(__name__)
+api = Api(app)
+
+def _get_random_num():
+    """
+    Return Random number between 5-30
+    """
+    return random.randint(5,30)
+
+
+def _get_mongodb_conn(mongo_host):
+    """
+    Get MongoDB connection
+    """
+    try:
+        print "Connecting to: %s" % mongo_host
+        mongo_conn = pymongo.MongoClient(mongo_host, connect=True, connectTimeoutMS=10000)
+        mongo_conn.server_info()
+        return mongo_conn
+    except pymongo.errors.ConnectionFailure, e:
+        print "Error Connecting to: %s" % e
+        return False
+
+def _connect_mongodb(mongo_host):
+    """
+    Connect to mongodb, return connection
+    """
+    print "Starting MongoDB connection"
+    mongo_conn = _get_mongodb_conn(mongo_host)
+    while not mongo_conn:
+            sleep_time = _get_random_num()
+            print "Problem Connecting to MongoDB, Waiting %s before trying again" % sleep_time
+            sleep(sleep_time)
+            mongo_conn = _get_mongodb_conn(mongo_host)
+    return mongo_conn
+
+def main():
+    print "%s (%s) starting up..." % (NAME, VERSION)
+    # maybe print some environment vars here...
+    # Testing connectiong to "mongo"
+    mongo_host = os.getenv('MONGO_URI', MONGO_URI)
+    print "Trying Connecting to MONGO_URI: %s" % mongo_host
+    mongo_conn = _connect_mongodb(mongo_host)
+    mdb_test = mongo_conn['test']
+    results = mdb_test.names.find_one()
+    print results['name']
+
+
+if __name__ == '__main__':
+    main()
